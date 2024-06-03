50f44cdfe683d67a1f2646a1ff5b838e7642637e
Andrew Barger <asbarg01@gmail.com>
2024-06-02 20:25:44 -0400

    Add non-sensitive data to azurerm backend config

--- src/backend.tf
+++ src/backend.tf
@@ -1,4 +1,12 @@
 terraform {
-  # Backend configuration will be provided as file. See https://developer.hashicorp.com/terraform/language/settings/backends/configuration#file
-  backend "azurerm" {}
+  # Sensitive backend configuration will be provided as file. See https://developer.hashicorp.com/terraform/language/settings/backends/configuration#file
+  backend "azurerm" {
+    storage_account_name = "sargroot1tf"
+    container_name       = "tfstate-container"
+    key                  = "terraform.tfstate"
+    #use_oidc signals we will use an SPN with Workload Identity Federation
+    use_oidc             = true
+    #use_azuread_auth signals the storage account uses Azure AD for authentication rather than an access key
+    use_azuread_auth     = true
+  }
 }
