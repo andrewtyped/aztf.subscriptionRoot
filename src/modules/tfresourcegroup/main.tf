3cc7d93ea03aca65d2e8ed1efa92a658cbab8a8c
Andrew Barger <asbarg01@gmail.com>
2024-06-05 23:37:28 -0400

    Progress on terraform-managed rg template

--- src/modules/tfresourcegroup/main.tf
+++ src/modules/tfresourcegroup/main.tf
@@ -1,4 +1,74 @@
+locals {
+  tags = {
+    environment = var.environment
+    department = var.department
+  }
+}
+
+# Resource Group
 resource "azurerm_resource_group" "tfmanaged" {
   name = "rg-${var.rg_topic}-${var.rg_increment}"
   location = var.rg_location
+  tags = local.tags
+}
+
+# Service Principal for Accessing tfstate
+resource "azuread_application_registration" "deployer_app" {
+    display_name = "${azurerm_resource_group.tfmanaged.name}-deployer"
+    description = "Used to deploy resources to ${azurerm_resource_group.tfmanaged.name} with Terraform."
+    sign_in_audience = "AzureADMyOrg"
+}
+
+resource "azuread_service_principal" "deployer_spn" {
+    client_id = azuread_application_registration.deployer_app.client_id
+    app_role_assignment_required = false
+}
+
+resource "azuread_application_federated_identity_credential" "deployer_spn_cred" {
+    application_id = azuread_application_registration.deployer_app.id
+    display_name = "${azuread_application_registration.deployer_app}-deployer-cred"
+    description = "Used to deploy resources to ${azurerm_resource_group.tfmanaged.name} with Terraform."
+    audiences = ["TODO"]
+    issuer = "TODO"
+    subject = "TODO"
+}
+
+
+# Storage Account for tfstate
+resource "azurerm_storage_account" "tfstate" {
+    name = "sa${var.rg_topic}tf${var.rg_increment}"
+    resource_group_name = azurerm_resource_group.tfmanaged.name
+    location = azurerm_resource_group.tfmanaged.location
+    account_tier = var.sa_tfstate_account_tier
+    account_replication_type = var.sa_tfstate_account_replication_type
+    account_kind = "StorageV2"
+    access_tier = "Hot"
+    # Network
+    public_network_access_enabled = var.sa_tfstate_public_network_access_enabled
+    enable_https_traffic_only = true
+    min_tls_version = "TLS1_2"
+
+    network_rules {
+      # Needs improvement, but no networks defined yet to play around with this
+      default_action = "Allow"
+    }
+
+    #Authorization
+    shared_access_key_enabled = false
+    default_to_oauth_authentication = true
+
+    
+    timeouts {
+      #Retain defaults
+      create = "60m"
+      update = "60m"
+      read = "5m"
+      delete = "60m"
+    }
+}
+
+resource "azurerm_storage_container" "tfstate_container" {
+    name = "tfstate-container"
+    storage_account_name = azurerm_storage_account.tfstate
+    container_access_type = "private"
 }
--- src/modules/tfresourcegroup/variables.tf
+++ src/modules/tfresourcegroup/variables.tf
@@ -1,3 +1,17 @@
+# Common
+
+variable "environment" {
+    description = "The name of the environment (dev, qa, prod) where the resource is deployed"
+    type = "string"
+    default = "dev"
+}
+
+variable "department" {
+    description = "The name of the business dept owning the resource"
+    type = "string"
+    default = "someco"
+}
+
 # Resource Group
 
 variable "rg_topic" {
@@ -15,4 +29,24 @@ variable "rg_location" {
     description = "The azure region where the group will be deployed"
     type = string
     default = "East US"
+}
+
+# Storage Account
+
+variable "sa_tfstate_account_tier" {
+    description = "Standard or Premium."
+    type = string
+    default = "Standard"
+}
+
+variable "sa_tfstate_account_replication_type" {
+    description = "Valid options are LRS, GRS, RAGRS, ZRS, GZRS, and RAGZRS. "
+    type = string
+    default = "LRS"
+}
+
+variable "sa_tfstate_public_network_access_enabled" {
+    description = "Is public network access enabled?"
+    type = bool
+    default = true
 }
